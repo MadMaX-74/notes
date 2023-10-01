4560f23f20bbc84792b45a558901e12fed9f1b25
MaxWebDev <maximus3506@mail.ru>
2023-10-01 19:17:30 +0300

    add request for note id

--- client/src/pages/note/[id].tsx
+++ client/src/pages/note/[id].tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import { NoteDocument } from '@/types/Note';
+import { getNoteById } from '@/services/api';
 
 const Note: React.FC = () => {
+  const [note, setNote] = useState<NoteDocument | null>(null)
   const router = useRouter();
   const { id } = router.query;
+  useEffect(() => {
+    if (id) {
+      const noteId = Array.isArray(id) ? id[0] : id;
+      getNote(noteId)
+    }
+  }, [id])
+  const getNote = async (id: string) => {
+    try {
+      const res = await getNoteById(id)
+    setNote(res!)
+    } catch (e){
+      console.error(e)
+    }
+  }
 
   return (
     <div className="text-center mt-20">
