de406f29e92f1adb45583d30b5dff3c5705e0c2c
MaxWebDev <maximus3506@mail.ru>
2023-10-07 22:06:43 +0300

    create note page

--- client/src/pages/note/[id].tsx
+++ client/src/pages/note/[id].tsx
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useId, useState } from 'react';
 import { useRouter } from 'next/router';
 import { NoteDocument } from '@/types/Note';
 import { getNoteById } from '@/services/api';
+import styles from '@/styles/Note.module.css'
 
-const Note: React.FC = () => {
-  const [note, setNote] = useState<NoteDocument | null>(null)
+const Note: React.FC = ({...props}): JSX.Element => {
+  const [note, setNote] = useState<NoteDocument>({
+    title: '',
+    content: ''
+  })
   const router = useRouter();
   const { id } = router.query;
   useEffect(() => {
@@ -15,16 +19,30 @@ const Note: React.FC = () => {
   }, [id])
   const getNote = async (id: string) => {
     try {
-      const res = await getNoteById(id)
-    setNote(res!)
-    } catch (e){
+      const res: any = await getNoteById(id)
+      res ? setNote(res?.note) : null
+    } catch (e) {
       console.error(e)
     }
   }
 
   return (
-    <div className="text-center mt-20">
-      <h1 className="text-3xl mb-4">Note {id}</h1>
+    <div className={styles.note} {...props}>
+      {note && <div className={styles.note_content}>
+        <span>Заголовок:</span>
+        <input className={styles.input}
+          type='text' value={note?.title || ''}
+          onChange={(e) => setNote({ ...note, title: e.target.value })} />
+        <span>Текст:</span>
+        <textarea className={styles.textarea}
+          name="content"
+          id='content'
+          maxLength={250}
+          value={note?.content || ''}
+          onChange={(e) => setNote(prevValue => ({ ...prevValue, content: e.target.value }))}></textarea>
+        <button className='btn common-btn' onClick={() => console.log(note)}>Сохранить</button>
+      </div>}
+      {!note && <div>No Data</div>}
     </div>
   );
 };
--- client/src/types/Note.ts
+++ client/src/types/Note.ts
@@ -2,4 +2,5 @@ export interface NoteDocument {
     _id?: string;
     title: string;
     content: string;
+    _v? :string
 }
